0d19c2ec13ed793f2c19373b49cb1a71bc3b5dcb
Mingcai SHEN <shen.mingcai@tvsee.cn>
2015-08-12 14:26:00 +0800

    Updated.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-sysctl vm.overcommit_memory=1
-echo never > /sys/kernel/mm/transparent_hugepage/enabled
+# sysctl vm.overcommit_memory=1
+# echo never > /sys/kernel/mm/transparent_hugepage/enabled
 
 if [ "$1" = 'redis-server' ]; then
 	chown -R redis .
