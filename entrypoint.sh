05ee8f15321ab42aa80beaaa2d368bd4a6a785d9
Mingcai SHEN <archsh@gmail.com>
2018-03-09 23:38:24 +0800

    Fixed start issue.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 # sysctl vm.overcommit_memory=1
 # echo never > /sys/kernel/mm/transparent_hugepage/enabled
 /usr/bin/gucci /usr/share/redis/redis.conf.template > /etc/redis/redis.conf
-if [ "$1" = 'redis-server' ]; then
-	chown -R redis /var/lib/redis
-	exec gosu redis "$@"
-fi
+# if [ "$1" = 'redis-server' ]; then
+# 	chown -R redis /var/lib/redis
+# 	exec gosu redis "$@"
+# fi
 
 exec "$@"
